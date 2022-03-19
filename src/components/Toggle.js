583082a2b5dd5cabda1ffd9f6ac960610922d835
Jarod Wilson <jaroddwilson@gmail.com>
2022-03-18 22:18:45 -0400

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [toggle, setToggle] = useState('OFF')
+
+  function swap() {
+    setToggle(toggle === 'OFF' ? 'ON' : 'OFF')
+  }
+
+  return <button style={{ background: `${toggle === 'OFF' ? 'white' : 'red'}`}} onClick={swap}>{toggle}</button>;
 }
 
 export default Toggle;
